994aef1aec8251f227a234f285b52eecc903d46b
IsmaBH <32642945+IsmaBH@users.noreply.github.com>
2025-02-17 18:42:07 -0600

    Code review exercise 3
    In this exercise I was given a code written by other person and was tasked to review it and correct any mistakes found.

---
+++ Ejercicios/codigo-otros-3/filtro.js
@@ -0,0 +1,69 @@
+// Tenemos un li de productos
+
+const productos = [
+  {nombre: "Zapato negro", tipo: "zapato", color: "negro", img: "./taco-negro.jpg"},
+  {nombre: "Zapato azul", tipo: "zapato", color: "azul", img: "./taco-azul.jpg"},
+  {nombre: "Bota negra", tipo: "bota", color: "negro", img: "./bota-negra.jpg"},
+  {nombre: "Bota azul", tipo: "bota", color: "azul", img: "./bota-azul.jpg"},
+  {nombre: "Zapato rojo", tipo: "zapato", color: "rojo", img: "./zapato-rojo.jpg"}
+]
+
+//Aqui era obtener el elemento mediante el id y no por nombre
+const li = document.getElementById("lista-de-productos")
+//Aqui esta el elemento pero no hay ningun elemento en el html que tenga esta descripcion
+//Lo cambie a busqueda ya que es mas descriptivo de lo es ese elemento
+const $i = document.querySelector('.busqueda');
+
+for (let i = 0; i < productos.length; i++) {
+  var d = document.createElement("div")
+  d.classList.add("producto")
+
+  var ti = document.createElement("p")
+  ti.classList.add("titulo")
+  ti.textContent = productos[i].nombre
+  
+  var imagen = document.createElement("img");
+  imagen.setAttribute('src', productos[i].img);
+
+  d.appendChild(ti)
+  d.appendChild(imagen)
+
+  li.appendChild(d)
+}
+
+//displayProductos(productos) que se pretende que realice la función displayProductos? el for anterior ya carga una cantidad
+//de productos por default
+//El boton de filtrado es mejor traerlo por medio de un id ya que despues podrian exisistir mas botones con diferente funcionalidad
+const botonDeFiltro = document.getElementById("btnFiltrar");
+
+botonDeFiltro.onclick = function() {
+  //
+  while (li.firstChild) {
+    li.removeChild(li.firstChild);
+  }
+
+  const texto = $i.value;
+  console.log(texto);
+  const productosFiltrados = filtrado(productos, texto );
+
+  for (let i = 0; i < productosFiltrados.length; i++) {
+    var d = document.createElement("div")
+    d.classList.add("producto")
+  
+    var ti = document.createElement("p")
+    ti.classList.add("titulo")
+    ti.textContent = productosFiltrados[i].nombre
+    
+    var imagen = document.createElement("img");
+    imagen.setAttribute('src', productosFiltrados[i].img);
+  
+    d.appendChild(ti)
+    d.appendChild(imagen)
+  
+    li.appendChild(d)
+  }
+}
+
+const filtrado = (productos = [], texto) => {
+  return productos.filter(item => item.tipo.includes(texto) || item.color.includes(texto));
+}  
